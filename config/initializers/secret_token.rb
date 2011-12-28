b9e42aa45def2fd8588bb7ce160cb62349225922
Timo Sand <timo.sand@kapsi.fi>
2011-12-28 20:13:26 +0200

    Redid the application with ActiveRecord and without Test::Unit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Roydon::Application.config.secret_token = '4e742bc1f873bda30d4197ea9e56497abedbe665e09fdcbc5476c0b0e90d4d77a0713fc31801d38ab4ec1f15852651700005186e064b45a1e0445b3b24b478ce'
+Roydon::Application.config.secret_token = '09507c14bc0f20a5637b93075d1b8659cde303557904481916ba6e77f983af23436c1bc9902ff3dc6ff8763ac696062d0409eacc00c29b28f8b9d277f5b88979'
