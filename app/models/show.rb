10ad9115dce46f2f81115038a7f1a65e29ad876d
Timo Sand <timo.sand@kapsi.fi>
2011-12-31 03:14:47 +0200

    Created Show -model

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,13 @@
 class Show
-	#  take some goodies from ActiveModel
-  	#  this will help us to seamlessly integrate this class into Rails
-	include ActiveModel::Validations
+	include MongoMapper::Document
 
-	
+	key :title, String
+	key :url, String
+	key :location, String
+	key :duration, Integer, :default => 1
+	key :date, Date, :default => ISODate('YYYY-MM-DD')
+	!timestamps
 
+	# Validations
+	validates_presence_of :title, :url, :location, :date
 end
