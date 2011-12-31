a749fb2ef0404ed81ec38a93ccef67d825245c23
Timo Sand <timo.sand@kapsi.fi>
2011-12-31 06:46:56 +0200

    Added helper to print show rows.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,20 @@
 module PagesHelper
+	
+	def display_show_rows(collection)
+		unless collection.blank?
+			html = ''
+		  	for show in collection
+		  		date = Time.parse(show.date.to_s)
+		    	html << "<tr id='#{date.strftime('%Y-%m-%d')}'>"
+		    	html << "	<td headers='show_time'>#{date.strftime('%d.%m.')}</td>"
+				html << "	<td headers='show_name'>"
+				html << "		<a href='#{show.url}'>#{show.title}</a>"
+				html << "	</td>"
+				html << "	<td headers='show_place'>#{show.location}</td>"
+				html << "</tr>"
+		  	end
+
+		  	return html.html_safe
+		end
+	end
 end
