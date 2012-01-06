943bd704c4123b034a600b2adf4a21a07371e911
Timo Sand <timo.sand@kapsi.fi>
2012-01-06 23:15:36 +0200

    Added helpers to generate year text and to generate previous year lists

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,4 +1,6 @@
 module PagesHelper
+
+	@@time = Date.today
 	
 	def display_show_rows(collection)
 		unless collection.blank?
@@ -16,5 +18,30 @@ module PagesHelper
 
 		  	return html.html_safe
 		end
+	end
+
+	def display_show_years(startYear = @@time.year)
+		if startYear < @@time.year
+			year_text = startYear
+		elsif @@time.month >= 6
+			year_text = "#{startYear} - #{@@time.next_year.year}"
+		else
+			year_text = startYear						
+		end
+	end
+
+	def display_year_list
+    	year = @@time.year
+    	prev_year = @@time.prev_year.year
+    	prev_prev_year = @@time.prev_year(2).year
+
+    	html = ''
+	    html << '<ul id="yearslist">'
+		html << '<li>' + link_to(year, shows_path(:year => year), :title => year) + '</li>'
+		html << '<li>' + link_to(prev_year, shows_path(:year => prev_year), :title => prev_year) + '</li>'
+		html << '<li>' + link_to(prev_prev_year, shows_path(:year => prev_prev_year), :title => prev_prev_year) + '</li>'
+		html << '</ul>'
+
+		html.html_safe
 	end
 end
