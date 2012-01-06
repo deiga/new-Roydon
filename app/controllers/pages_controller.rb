a3fa532b58f8d8cb9d00d3a638a6bc8ca491c154
Timo Sand <timo.sand@kapsi.fi>
2012-01-06 23:11:53 +0200

    Added parameter handling. Refactored queries. Added query for earlier years

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,8 +15,18 @@ class PagesController < ApplicationController
 
   def shows
   	@title = 'Shows'
-  	@earlier_shows = Show.all( :date.lt => Date.today.to_mongo )
-    @upcoming_shows = Show.all( :date.gte => Date.today.to_mongo )
+    @year = params[:year].to_i
+    today = Date.today
+  	@earlier_shows = Show.all( :date.lt => today.to_mongo, 
+      :date.gte => Date.new(today.year).to_mongo)
+    
+    if @year < today.year
+      queried_year = Date.new(@year)
+      @upcoming_shows = Show.all( :date.gte => queried_year.to_mongo, 
+        :date.lt => queried_year.next_year.to_mongo)
+    else
+      @upcoming_shows = Show.all( :date.gte => today.to_mongo )
+    end
   end
 
 end
