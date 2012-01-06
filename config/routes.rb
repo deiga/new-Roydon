fbf8db6f6a4a16bcba7f218e55cfc1c2132a1b0b
Timo Sand <timo.sand@kapsi.fi>
2012-01-06 23:06:52 +0200

    Removed index.html Refactored routes to be able to choose year of shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,26 @@
 Roydon::Application.routes.draw do
 
   get "shows/index"
-
   get "shows/create"
-
   get "shows/edit"
-
   get "shows/update"
-
   get "shows/destroy"
 
   match '/contact', :to => 'pages#contact'
-  match '/home', :to => 'pages#home'
   match '/about', :to => 'pages#about'
-  match '/shows', :to => 'pages#shows'
+  match '/shows(/:year)', :to => 'pages#shows', 
+    :constraints => { :year => /(\d\d\d\d)/ },
+    :as => 'shows',
+    :defaults => {:year => 2012}
 
   root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Sample resource route with options:
   #   resources :products do
@@ -64,13 +55,6 @@ Roydon::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
   # See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
