74b1f333da6cfbcae17f0ee231ae187c0dabfdee
Timo Sand <timo.sand@kapsi.fi>
2012-03-11 00:19:39 +0200

    Moved pages#shows into shows#index

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,17 +1,18 @@
 class ShowsController < ApplicationController
   def index
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    @title = 'Shows'
+    @year = params[:year].to_i
+    today = Date.today
+    @earlier_shows = Show.where( :date.lt => today, 
+          :date.gte => Date.new(today.year) )
+    
+    if @year < today.year
+      queried_year = Date.new(@year)
+      @upcoming_shows = Show.where( :date.gte => queried_year, 
+              :date.lt => queried_year.next_year )
+    else
+      @upcoming_shows = Show.where( :date.gte => today )
+    end
   end
 
 end
