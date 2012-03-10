aa22f646de773b99ec3cfc2bfd2b3a8ca8146c24
Timo Sand <timo.sand@kapsi.fi>
2012-03-10 23:01:13 +0200

    Embedded categories into top_category and created a many-to-many relation to products

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,9 @@ class Category
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  embedded_in :top_category
+  has_and_belongs_to_many :products
+
   field :name, :type => String
   field :passive, :type => Boolean, :default => false
 
