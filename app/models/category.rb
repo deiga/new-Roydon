03639383334baa4fb5d0c4b8a97ee5c1ce1e5caf
Timo Sand <timo.sand@kapsi.fi>
2012-03-11 04:10:06 +0200

    Added tests and fixes for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,11 +2,11 @@ class Category
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  embedded_in :top_category
+  belongs_to :top_category
   has_and_belongs_to_many :products
 
   field :name, :type => String
   field :passive, :type => Boolean, :default => false
 
-  validates :name, :presence => true
+  validates :name, :presence => true, :length => { :minimum => 1 }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @attr = { :name => 'Test category'}
+  end
+
+  it "should create a new Category given valid attributes" do
+    created = Category.create!(@attr)  
+  end
+
+  it "should be empty" do
+    all_categories = Category.all
+    all_categories.should be_empty
+  end
+
+  it "should not accept zero length name" do
+    no_name_category = Category.new(@attr.merge(:name => ''))
+    no_name_category.should_not be_valid
+  end
+
+  it "should have a name" do
+    no_name_category = Category.new
+    no_name_category.should_not be_valid
+  end
 end
