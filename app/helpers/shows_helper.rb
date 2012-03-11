15e62b3bc005d45c53699ffe39dec37eb346bda4
Timo Sand <timo.sand@kapsi.fi>
2012-03-11 04:09:19 +0200

    Added setter and getter for @@time. Reformatted code. Added documentation

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,25 +1,36 @@
 module ShowsHelper
 
   @@time = Date.today
+
+  def time
+    @@time
+  end
+
+  def time=(value)
+    @@time = value if value.kind_of? Date
+  end
   
   def display_show_rows(collection)
     unless collection.blank?
       html = ''
-        for show in collection
-          html << "<tr id='#{show.date}'>"
-          html << " <td headers='show_time'>#{show.format_date}</td>"
+      for show in collection
+        html << "<tr id='#{show.date}'>"
+        html << " <td headers='show_time'>#{show.format_date}</td>"
         html << " <td headers='show_name'>"
         html << "   <a href='#{show.url}'>#{show.title}</a>"
         html << " </td>"
         html << " <td headers='show_place'>#{show.location}</td>"
         html << "</tr>"
-        end
+      end
 
-        return html.html_safe
+      return html.html_safe
     end
   end
 
-  def display_show_years(startYear = @@time.year)
+  # Output the year range for the displayed shows
+  # * If the given year is a past year, then return given
+  # * If this year is beyond June, return range of this year to next year
+  def display_show_year(startYear = @@time.year)
     if startYear < @@time.year
       year_text = startYear
     elsif @@time.month >= 6
@@ -30,14 +41,14 @@ module ShowsHelper
   end
 
   def display_year_list
-      year = @@time.year
-      prev_year = @@time.prev_year.year
-      prev_prev_year = @@time.prev_year(2).year
+    year = @@time.year
+    prev_year = @@time.prev_year.year
+    prev_prev_year = @@time.prev_year(2).year
 
-      html = ''
-      html << '<ul id="yearslist">'
-      html << '<li>' + link_to(prev_prev_year, shows_path(:year => prev_prev_year), :title => prev_prev_year) + '</li>'
-      html << '<li>' + link_to(prev_year, shows_path(:year => prev_year), :title => prev_year) + '</li>'
+    html = ''
+    html << '<ul id="yearslist">'
+    html << '<li>' + link_to(prev_prev_year, shows_path(:year => prev_prev_year), :title => prev_prev_year) + '</li>'
+    html << '<li>' + link_to(prev_year, shows_path(:year => prev_year), :title => prev_year) + '</li>'
     html << '<li>' + link_to(year, shows_path(:year => year), :title => year) + '</li>'
     html << '</ul>'
 
