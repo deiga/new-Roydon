18940d036590d4c1550c6c7800ae95486217709f
Timo Sand <timo.sand@kapsi.fi>
2012-03-11 14:15:15 +0200

    Added seed data for usergroups and topcategories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+UserGroup.create( :name => 'Breeder' )
+UserGroup.create( :name => 'Groomer' )
+
+['Turkinhoitoaineet', 'Turkinhoitotarvikkeet', 'Koulutus', 'Syötävät',
+ 'Taluttimet', 'Pannat', 'Valjaat', 'Häkit, boxit, kantolaukut',
+ 'Makuualustat', 'Trimmauspöydät',
+ 'Vetoalustat, pentuaitaukset, portit', 'Lelut', 'Muut'].
+each do |name|
+  TopCategory.create(:name => name)
+end
