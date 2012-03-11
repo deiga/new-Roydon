d41709d7b1b128946438738ea575a0b461f14005
Timo Sand <timo.sand@kapsi.fi>
2012-03-11 14:16:07 +0200

    Added discounts

---
+++ app/models/discount.rb
@@ -0,0 +1,11 @@
+class Discount
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :name, :type => String
+  field :amount, :type => Float
+
+  has_many :products
+  has_many :categories
+  belongs_to :user_group
+end
