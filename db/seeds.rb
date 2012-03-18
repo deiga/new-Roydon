7cb2a769bee166972c448f9cf88b4714d6a53fc9
Timo Sand <timo.sand@kapsi.fi>
2012-03-19 00:14:33 +0200

    Reworked seeding of categories and refactored into functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,50 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
+def create_categories
+  categories_yaml = YAML.load_file('db/seed/categories.yml')
+  categories_yaml.each do |top_category, category_list|
+    parent = TopCategory.new(:name => top_category)
+    parent.save!
+
+    unless category_list.nil?
+      category_list.each do |category|
+        parent.categories.create!(:name => category)
+      end
+    end
+  end
+end
+
+def create_shows
+  Ccsv.foreach('db/seed/shows.csv') do |line|
+    line.each { |str| str.gsub!(/\"/,'') }
+    title = line[3]
+    len = line.length
+    if len > 5
+      1.upto(len-4) { |i| title += line[3+i]}
+    end
+    if len < 5 
+      url = nil
+    else
+      url = line[-1]
+    end
+    date = Date.strptime(line[1])
+    Show.create!(:location => line[0], :date => date,
+      :duration => line[2], :title => line[3], :url => url)
+  end
+end
+
+def create_stories
+  Ccsv.foreach('db/seed/news.csv') do |line|
+    line.each { |str| str.gsub!(/\"/,'').gsub!(/;/,',') }
+    date = Date.strptime(line[0])
+    Story.create!(:date => date, :title => line[1],
+      :content => line[2])
+    Story.create!(:date => date, :title => line[3],
+      :content => line[4], :language => 'en')
+  end
+end
+
 UserGroup.create( :name => 'Breeder' )
 UserGroup.create( :name => 'Groomer' )
 
@@ -16,35 +60,7 @@ if Rails.env.development?
   #Admin.create!(:email => 'webmaster@roydon.fi', :password => '')
 end
 
-['Turkinhoito', 'Trimmaus', 'Hyvinvointi' 'Koulutus', 'Syötävät', 
- 'Ruokakupit', 'Taluttimet', 'Pannat', 'Valjaat & Liivit', 'Häkit',
- 'Makuualustat', 'Aitaukset & portit', 'Lelut', 'Muut'].
- each do |name|
-  TopCategory.create(:name => name)
-end
+create_categories()
+create_shows()
+create_stories()
 
-Ccsv.foreach('db/seed/shows.csv') do |line|
-  line.each { |str| str.gsub!(/\"/,'') }
-  title = line[3]
-  len = line.length
-  if len > 5
-    1.upto(len-4) { |i| title += line[3+i]}
-  end
-  if len < 5 
-    url = nil
-  else
-    url = line[-1]
-  end
-  date = Date.strptime(line[1])
-  Show.create!(:location => line[0], :date => date,
-    :duration => line[2], :title => line[3], :url => url)
-end
-
-Ccsv.foreach('db/seed/news.csv') do |line|
-  line.each { |str| str.gsub!(/\"/,'').gsub!(/;/,',') }
-  date = Date.strptime(line[0])
-  Story.create!(:date => date, :title => line[1],
-    :content => line[2])
-  Story.create!(:date => date, :title => line[3],
-    :content => line[4], :language => 'en')
-end
