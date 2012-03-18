0dec40613f75ae3d4c0d7c52641e86bfb982a138
Timo Sand <timo.sand@kapsi.fi>
2012-03-19 01:05:53 +0200

    Added TODO for language of Story

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
   	@title = 'Home'
+    # TODO: Add filtering by current language
     @next_show = Show.asc(:date).where( :date.gte => Date.today ).first()
     @index_news = Story.where( :date.gte => Date.today.prev_month(3) )
   end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,8 @@
 class StoriesController < ApplicationController
   def index
     @title = 'News'
-    @all_news = Story.desc(:Date).all();
+    @all_news = Story.desc(:Date).all()
+    # TODO: Add filtering by current language
   end
 
 end
