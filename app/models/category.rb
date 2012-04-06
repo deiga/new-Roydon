34d6b6a02b3a5c3d41dc2c810e52964c1941bb86
Timo Sand <timo.sand@kapsi.fi>
2012-04-06 15:18:58 +0300

    Implemented ancestry of categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,17 @@
 class Category
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Ancestry
 
-  belongs_to :top_category
+  has_ancestry
   has_and_belongs_to_many :products
 
   field :name, :type => String
   field :passive, :type => Boolean, :default => false
 
   validates :name, :presence => true, :length => { :minimum => 2 }
+
+  def top_categories
+    Category.all(:ancestry => nil)
+  end
 end
