00fd6f7fc50c596d4acfcb35c90f07be5bb2d898
Timo Sand <timo.sand@kapsi.fi>
2012-04-07 21:07:00 +0300

    REFACTORED: Shop controller to list products in current category and to show category lists for top categories

--- app/controllers/shop/shop_controller.rb
+++ app/controllers/shop/shop_controller.rb
@@ -1,6 +1,15 @@
 class Shop::ShopController < ApplicationController
   def home
-    @top_categories = TopCategory.where(:passive => false)
+    @top_categories = Category.top_categories
+    @top_category = Category.find_by_formatted_name params[:top_category] unless params[:top_category].nil?
+    @categories = @top_category.children unless @top_category.nil?
+    @category = Category.find_by_formatted_name params[:category] unless params[:category].nil?
+    
+    unless @category.nil?
+      @products = @category.products
+    else
+      @products = @top_category.products unless @top_category.nil?
+    end
   end
 
 end
