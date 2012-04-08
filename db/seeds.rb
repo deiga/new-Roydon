c706ee277faa7abeabecc8a7a61e1e87aaded514
Timo Sand <timo.sand@kapsi.fi>
2012-04-08 21:13:20 +0300

    CHANGED: Seeds to add 15 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,8 +81,11 @@ create_categories()
 create_shows()
 create_stories()
 
-test_product = Product.create!(:name => 'Kevytmetallihäkki L', :price => '65', :description => 
-  "Paino: 5kg<br />93p 57l 62k<br />2-ovinen, muovipohja<br />Saatavana 7 eri värissä.",
-  :image => "http://www.tujomakauppa.net/kuvat/TU4135.jpg")
-test_product.categories << Category.any_in(:name => ['Häkit'])
+1.upto(15) do |i|
+  test_product = Product.create!(:name => 'Kevytmetallihäkki L', :price => '65', :description => 
+    "Paino: 5kg<br />93p 57l 62k<br />2-ovinen, muovipohja<br />Saatavana 7 eri värissä.",
+    :image => "http://www.tujomakauppa.net/kuvat/TU4135.jpg")
+  test_product.categories << Category.any_in(:name => ['Häkit'])
+end
+
 
