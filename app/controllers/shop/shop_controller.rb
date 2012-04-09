4c1b3336c30972d7e1845f884aa9380d28be5a2f
Timo Sand <timo.sand@kapsi.fi>
2012-04-09 18:45:45 +0300

    ADDED: reading cart from session

--- app/controllers/shop/shop_controller.rb
+++ app/controllers/shop/shop_controller.rb
@@ -1,9 +1,12 @@
 class Shop::ShopController < ApplicationController
+  
   def home
     @top_categories = Category.top_categories
     @top_category = Category.find_by_formatted_name params[:top_category] unless params[:top_category].nil?
     @categories = @top_category.children unless @top_category.nil?
     @category = Category.find_by_formatted_name params[:category] unless params[:category].nil?
+    session[:shopping_cart] ||= ShoppingCart.new
+    @shopping_cart = session[:shopping_cart]
     
     unless @category.nil?
       @products = @category.products.page(params[:page])
