fd719c4a49e2f9b7c3efed94fa5159d387ca35d6
Timo Sand <timo.sand@kapsi.fi>
2012-04-10 17:45:03 +0300

    REFACTORED: specs to correctly test the application

--- spec/models/shopping_cart_spec.rb
+++ spec/models/shopping_cart_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe ShoppingCart do
   
-  describe "Emptying" do
+  describe "calling empty" do
     it "should do nothing on an empty cart" do
-      empty_shopping_cart = ShoppingCart.new
+      empty_shopping_cart = ShoppingCart.create!
       empty_shopping_cart.empty
       empty_shopping_cart.size.should be 0
     end
 
     it "should empty an non-empty cart" do
-      cart = ShoppingCart.new
-      cart.add Product.new
+      cart = ShoppingCart.create!
+      cart.add Product.create!(:name => 'Test 42', :price => 2.3)
       cart.size.should be 1
       cart.empty
       cart.size.should be 0
