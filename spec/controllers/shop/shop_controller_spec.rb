56d59d21699bb9d2f5ffdf69cd224dd3490432fe
Timo Sand <timo.sand@kapsi.fi>
2012-04-10 17:45:46 +0300

    ADDED: test for price calculation

--- spec/controllers/shop/shop_controller_spec.rb
+++ spec/controllers/shop/shop_controller_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe Shop::ShopController do
-  render_views
+	render_views
 
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
-  end
+	describe "GET 'index'" do
+		it "returns http success" do
+			get 'index'
+			response.should be_success
+		end
+
+		it "should call the model price calculation" do
+			@shopping_cart = session[:shopping_cart] ||= ShoppingCart.new 
+			@shopping_cart.add Product.create(:price => 5.3)
+			@shopping_cart.should_receive(:price)
+			get 'index'
+		end
+	end
 
 end
