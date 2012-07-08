a3497f198892484f67b5b88741f1e33e43cf1926
Timo Sand <timo.j.sand@gmail.com>
2012-07-08 14:01:22 +0300

    REFACTORED: To use story partial

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,17 +1,3 @@
 module PagesHelper
 
-	def display_news_items(collection)
-		unless collection.blank?
-			html = '<div class="scroller">'
-			for story in collection
-				html << "<section class='news'>"
-				html << "	<h4>#{story.title}</h4><span> - #{story.format_date}</span>"
-				html << "	<p>#{story.content}</p>"
-				html << "</section>"
-			end
-			html << '</div>'
-
-			return html.html_safe
-		end
-	end
 end
