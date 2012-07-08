c4d7877ff3b70ccb9aec2cbceaaed58fd5c8e7bd
Timo Sand <timo.j.sand@gmail.com>
2012-07-09 00:18:11 +0300

    CHANGED: To inherit from ShopController

--- app/controllers/shop/products_controller.rb
+++ app/controllers/shop/products_controller.rb
@@ -1,5 +1,8 @@
-class Shop::ProductsController < ApplicationController
+class Shop::ProductsController < Shop::ShopController
+  
   def index
+    @category = Category.where(:permalink => params[:category]).first
+    @products = @category.all_products.order('name').page(params[:page])
   end
 
   def show
