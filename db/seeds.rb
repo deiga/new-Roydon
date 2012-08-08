32d2f13a60b392504cd307cdbdea108b1c221f49
Timo Sand <timo.j.sand@gmail.com>
2012-08-09 00:28:16 +0300

    ADDED: News and shows seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+puts "Seeding for env '#{Rails.env}'"
+
+def create_shows
+  shows = YAML.load_file('db/seed/shows.yml')
+  shows.each do |show|
+    date = Date.strptime(show['aika'])
+    Show.find_or_create_by(location: show['paikka'], date: date, duration: show['kesto'], title: show['nimi'], url: show['link'])
+  end 
+end
+
+def create_stories
+  news = YAML.load_file('db/seed/news.yml')
+  news.each do |news_item|
+    date = Date.strptime(news_item['date'])
+    Story.find_or_create_by(date: date, title: news_item['head'], content: news_item['content'])
+    Story.find_or_create_by(date: date, title: news_item['headEn'], content: news_item['contEn'], language: 'en')
+  end 
+end
+
+
+p "Creating shows"
+create_shows
+p "Creating stories"
+create_stories
+
+p "All done!"
