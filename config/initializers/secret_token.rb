be4384861ca74e5d44ddf0aa5c09847137dfbf99
Timo Sand <timo.j.sand@gmail.com>
2012-08-14 23:01:31 +0300

    CHANGED: Cachebusting

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Roydon::Application.config.secret_token = '09507c14bc0f20a5637b93075d1b8659cde303557904481916ba6e77f983af23436c1bc9902ff3dc6ff8763ac696062d0409eacc00c29b28f8b9d277f5b88979'
+Roydon::Application.config.secret_token = '19507c14bc0f20a5637b93075d1b8659cde303557904481916ba6e77f983af23436c1bc9902ff3dc6ff8763ac696062d0409eacc00c29b28f8b9d277f5b88979'
