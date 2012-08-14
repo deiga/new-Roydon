59a5f11670346c24978e934b1ef5ebece043440c
Timo Sand <timo.j.sand@gmail.com>
2012-08-14 21:23:34 +0300

    FIXED: Session getting too big

--- app/controllers/shop/shop_controller.rb
+++ app/controllers/shop/shop_controller.rb
@@ -2,11 +2,15 @@ class Shop::ShopController < ApplicationController
   layout 'shop'
   
   def index
-    @top_categories = Category.top_categories
-    @top_category = Category.find_by_formatted_name params[:top_category] unless params[:top_category].nil?
+    session[:top_category_ids] = Category.top_categories.map(&:id).map(&:to_s)
+    @top_category = Category.find_by id: params[:top_category_id] unless params[:top_category_id].nil?
     @categories = @top_category.children unless @top_category.nil?
     @category = Category.find_by_formatted_name params[:category] unless params[:category].nil?
-    @shopping_cart = session[:shopping_cart] ||= ShoppingCart.new
+    @shopping_cart = ShoppingCart.find_or_create_by(id: session[:shopping_cart_id])
+    session[:shopping_cart_id] = @shopping_cart.id
+  end
+
+  def add_to_cart
   end
 
 end
--- app/helpers/shop/shop_helper.rb
+++ app/helpers/shop/shop_helper.rb
@@ -1,5 +1,6 @@
 module Shop::ShopHelper
-  def category_link(category)
+  def category_link(category_id)
+    category = Category.find(category_id)
     link_to raw(category.name.gsub(/\s/, '&nbsp;')), category_shop_products_path(category), :id => category.format_name
   end
 end
