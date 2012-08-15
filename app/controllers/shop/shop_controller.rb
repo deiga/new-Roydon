8c33b65f8d7d067addfe026ac9291da6c772af60
Timo Sand <timo.j.sand@gmail.com>
2012-08-15 23:42:08 +0300

    FIXED: Shopping cart instantiation and recalling

--- app/controllers/shop/shop_controller.rb
+++ app/controllers/shop/shop_controller.rb
@@ -1,35 +1,36 @@
 # encoding: UTF-8
 class Shop::ShopController < ApplicationController
   layout 'shop'
+  before_filter :set_cart
 
   def index
     session[:top_category_ids] = Category.top_categories.map(&:id).map(&:to_s)
     @top_category = Category.find_by id: params[:top_category_id] unless params[:top_category_id].nil?
     @categories = @top_category.children unless @top_category.nil?
     @category = Category.find_by_formatted_name params[:category] unless params[:category].nil?
-    begin
-      @shopping_cart = (session[:shopping_cart_id].nil? && ShoppingCart.create) || ShoppingCart.find(session[:shopping_cart_id])
-    rescue Mongoid::Errors::DocumentNotFound => e
-      @shopping_cart = ShoppingCart.create
-    end
-    session[:shopping_cart_id] = @shopping_cart.id.to_s
+    
   end
 
   def add_to_cart
     # raise session.inspect
-    begin
-      shopping_cart = (session[:shopping_cart_id].nil? && ShoppingCart.create) || ShoppingCart.find(session[:shopping_cart_id])
-    rescue Mongoid::Errors::DocumentNotFound => e
-      shopping_cart = ShoppingCart.create
-    end
-    session[:shopping_cart_id] = shopping_cart.id.to_s
     product = Product.find(params[:product])
-    shopping_cart.add(product)
+    @shopping_cart.add(product)
 
     respond_to do |format|
       format.html { flash[:notice] = 'Tuote lisätty onnistuneesti'; redirect_to(request.referer ||shop_path) }
       format.json  {render :json => { message: 'Tuote lisätty onnistuneesti' } }
     end
   end
+
+  private
+
+    def set_cart
+      begin
+      @shopping_cart = (session[:shopping_cart_id].nil? && ShoppingCart.create) || ShoppingCart.find(session[:shopping_cart_id])
+    rescue Mongoid::Errors::DocumentNotFound => e
+      @shopping_cart = ShoppingCart.create
+    end
+    session[:shopping_cart_id] = @shopping_cart.id.to_s
+    end
 
 end
