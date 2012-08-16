78eb79e65041d1cabf3981526c9622a0d77fe7df
Timo Sand <timo.j.sand@gmail.com>
2012-08-16 22:00:33 +0300

    REMOVED: Stock from product

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,17 +10,11 @@ class Product
 
   field :name,        :type => String
   field :price,       :type => BigDecimal,  :default => 0.0
-  field :stock,       :type => Integer,     :default => 0
   field :passive,     :type => Boolean,     :default => false
   field :suggestion,  :type => Boolean,     :default => false
   field :description, :type => String
   field :image,       :type => String
 
-  def in_stock?
-    return self.stock > 0
-  end
-
   validates :name, :presence => true, :length => { :minimum => 1 }
   validates :price, :numericality => { :greater_than_or_equal_to => 0.0}
-  validates :stock, :numericality => { :only_integer => true }
 end
