4face8915cf38a8afb226d8f8760717c0bab4272
Timo Sand <timo.j.sand@gmail.com>
2012-08-20 16:56:01 +0300

    CHANGED: Moved bullet config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,15 +32,4 @@ Roydon::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.growl = true
-    Bullet.rails_logger = true
-    Bullet.disable_browser_cache = true
-  end
-
 end
