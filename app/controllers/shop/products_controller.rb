5509c88dc60418a745b7206f42995e49f0c5d5ab
Timo Sand <timo.j.sand@gmail.com>
2012-08-20 16:56:27 +0300

    ADDED: Eager loading to products

--- app/controllers/shop/products_controller.rb
+++ app/controllers/shop/products_controller.rb
@@ -2,7 +2,7 @@ class Shop::ProductsController < Shop::ShopController
   
   def index
     @category = Category.where(:permalink => params[:category]).first
-    @products = @category.all_products.asc('name').page(params[:page]) unless @category.nil?
+    @products = @category.all_products.asc('name').includes(:options).page(params[:page]) unless @category.nil?
   end
 
   def show
