26d4cca973c4af3f13d232286d2ce3be9c9404bc
Timo Sand <timo.sand@ipss.fi>
2012-08-20 08:33:31 +0300

    REFACTORED: User groups to include only pre-defined groups

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -4,6 +4,8 @@ class UserGroup
 
   field :name, :type => String
 
+  validates :name, presence: true, inclusion: { in: %w{admin breeder groomer}}, uniqueness: true
+
   has_and_belongs_to_many :users
   has_many :discounts
 end
