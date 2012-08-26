6acf1bb4248251d1748aa6158bf51f10e8b145ce
Timo Sand <timo.j.sand@gmail.com>
2012-08-26 14:15:48 +0300

    FIXED: database_cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,11 +11,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   # == Mock Framework
   #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -34,8 +29,7 @@ RSpec.configure do |config|
   # Clean up the database
   require 'database_cleaner'
   config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.orm = "mongoid"
+    DatabaseCleaner[:mongoid].strategy = :truncation
   end
 
   config.before(:each) do
