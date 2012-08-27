8064a96d76fbdff34090b8985524ecfc18d4c6ee
Timo Sand <timo.j.sand@gmail.com>
2012-08-27 21:57:48 +0300

    ADDED: after filter to move flash to XHR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :flash_to_headers
 
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
+
+  private
+
+    def flash_to_headers
+      return unless request.xhr?
+      response.headers['X-Message-Type'] = flash.keys.first.to_s
+      response.headers['X-Message'] = flash[:notice] unless flash[:notice].blank?
+      response.headers['X-Message'] = flash[:error] unless flash[:error].blank?
+
+      flash.discard  # don't want the flash to appear when you reload page
+    end
 end
