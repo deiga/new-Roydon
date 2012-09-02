629d35b72fe25ff1d1ab238e1c0b8bfb22e914ba
Timo Sand <timo.j.sand@gmail.com>
2012-09-02 22:23:53 +0300

    ADDED: OrderItems

---
+++ app/models/order_item.rb
@@ -0,0 +1,16 @@
+class OrderItem
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  embedded_in :order
+
+  field :product_id, type: Moped::BSON::ObjectId
+  field :product_name, type: String
+  field :product_price, type: BigDecimal, default: 0.0
+  field :quantity, type: Integer, default: 1
+  field :options, type: String
+
+  validates :name, :presence => true, :length => { :minimum => 1 }
+  validates :price, :numericality => { :greater_than_or_equal_to => 0.0}
+  validates :quantity, :numericality => { :greater_than_or_equal_to => 0 }
+end
