86df2020a5a6652f012c150199ae2a8ae4ee88ca
Timo Sand <timo.j.sand@gmail.com>
2012-09-14 01:06:38 +0300

    ADDED: products factory

---
+++ spec/factories/products.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do
+
+  trait :test_file do
+    image { fixture_file_upload 'files/test%image 1.jpg' }
+  end
+
+  factory :product do
+    name "Test product"
+  end
+
+  # This will use the User class (Admin would have been guessed)
+  factory :product_with_image, class: Product do
+    name "Test product with image"
+    test_file
+
+    # after_create do |product, proxy|
+    #   proxy.file.close
+    # end
+  end
+
+
+end
