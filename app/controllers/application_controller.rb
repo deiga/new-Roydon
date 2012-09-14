0650b56bd0ae09c04357d0dd5bd913e3657bdbcf
Timo Sand <timo.j.sand@gmail.com>
2012-09-14 15:11:54 +0300

    ADDED: Redirect to error page on error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   after_filter :flash_to_headers
+  rescue_from ActionController::RoutingError, :with => :render_404
 
   def not_found
     raise ActionController::RoutingError.new('Not Found')
@@ -15,5 +16,13 @@ class ApplicationController < ActionController::Base
       response.headers['X-Message'] = flash[:error] unless flash[:error].blank?
 
       flash.discard  # don't want the flash to appear when you reload page
+    end
+
+    def render_404(exception = nil)
+      if exception
+          logger.info "Rendering 404: #{exception.message}"
+      end
+
+      render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
     end
 end
