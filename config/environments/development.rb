27123c9d6f891ec2748cb9495ff5a6454937d80e
Timo Sand <timo.j.sand@gmail.com>
2012-11-18 10:50:58 +0000

    FIXED: Wrong URL for S3 storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,5 @@ Roydon::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   # S3 config for paperclip
-  config.paperclip_defaults = {:storage => :s3, :s3_credentials => {access_key_id: ENV['S3_ACCESS'], secret_access_key: ENV['S3_SECRET']}, bucket: "roydon-dev", path: ":attachment/:id/:style.:extension"}
+  config.paperclip_defaults = {:storage => :s3, :s3_credentials => {access_key_id: ENV['S3_ACCESS'], secret_access_key: ENV['S3_SECRET']}, bucket: "roydon-dev", path: ":attachment/:id/:style.:extension", url: :s3_domain_url}
 end
