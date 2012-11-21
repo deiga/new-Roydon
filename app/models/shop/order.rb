42be5931e197848fc895a6491aa84f58029af3eb
Timo Sand <timo.j.sand@gmail.com>
2012-11-21 12:43:14 +0000

    CHANGED: Replaced belonging to user with fillable details for the order

--- app/models/shop/order.rb
+++ app/models/shop/order.rb
@@ -2,9 +2,24 @@ class Shop::Order
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  PAYMENT_TYPES = ['Credit card', 'Cash']
+
   embeds_many :items,class_name: 'Shop::OrderItem', inverse_of: :order
   accepts_nested_attributes_for :items
-  belongs_to :user
+  # belongs_to :user
+
+  field :name, type: String
+  field :email, type: String
+  field :address, type: String
+  field :country, type: String
+  field :city, type: String
+  field :postal_number, type: String
+  field :phone, type: String
+  field :message, type: String
+  field :payment, type: String
+
+  validates :name, :address, :email, presence: true
+  validates :payment, inclusion: PAYMENT_TYPES
 
   def add_item(cart_item)
     product = cart_item.product
