4cf49b44cc50204739814ea3f3aaca7313a5fb46
Timo Sand <timo.j.sand@gmail.com>
2012-11-21 12:32:29 +0000

    REFACTORED: Allowed groups into global array

--- app/models/shop/user_group.rb
+++ app/models/shop/user_group.rb
@@ -2,9 +2,11 @@ class Shop::UserGroup
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  GROUP_TYPES = ['admin', 'breeder', 'groomer']
+
   field :name, :type => String
 
-  validates :name, presence: true, inclusion: { in: %w{admin breeder groomer}}, uniqueness: true
+  validates :name, presence: true, inclusion: GROUP_TYPES, uniqueness: true
 
   has_and_belongs_to_many :users
   has_many :discounts, class_name: 'Shop::Discount'
