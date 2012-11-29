fc42eae41917db4aaab0c3d7dc57cfddb4c7a791
Timo Sand <timo.j.sand@gmail.com>
2012-11-29 03:06:35 +0000

    CHANGED: Removed default scoping with include

--- app/models/shop/cart_item.rb
+++ app/models/shop/cart_item.rb
@@ -7,7 +7,7 @@ class Shop::CartItem
   belongs_to :cart, class_name: 'Shop::ShoppingCart', inverse_of: 'items'
   belongs_to :product, class_name: 'Shop::Product'
 
-  default_scope includes(:product)
+  # default_scope includes(:product)
 
   field :selected_option, type: Array, default: []
   field :quantity, type: Integer, default: 1
