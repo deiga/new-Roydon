094b39d91fa36518acf6a6f75df86b18b53d4748
Timo Sand <timo.j.sand@gmail.com>
2012-11-29 03:03:48 +0000

    CHANGED: Config for spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  # config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -31,9 +31,14 @@ RSpec.configure do |config|
   require 'database_cleaner'
   config.before(:suite) do
     DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after do
     DatabaseCleaner.clean
   end
 
