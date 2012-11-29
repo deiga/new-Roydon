a5d9b43ea416bc4d1d6f195ec8d83b0e6dee2e4a
Timo Sand <timo.j.sand@gmail.com>
2012-11-29 03:07:47 +0000

    CHANGED: Optimized a N+1 Query

--- app/controllers/shop/products_controller.rb
+++ app/controllers/shop/products_controller.rb
@@ -2,7 +2,7 @@ class Shop::ProductsController < Shop::ShopController
 
   def index
     category_params = params[:category] || ''
-    @category = Shop::Category.where(:permalink => category_params.split('~').last).first
+    @category = Shop::Category.includes(:products).where(:permalink => category_params.split('~').last).first
     @products = @category.all_products.asc('name').includes(:options).page(params[:page]) unless @category.nil?
   end
 
