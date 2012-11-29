5bd3bcc14ffa9fcd8b4272e234943802e576fb56
Timo Sand <timo.j.sand@gmail.com>
2012-11-29 03:09:49 +0000

    CHANGED: Improvements to shopping cart specs

--- spec/models/shop/shopping_cart_spec.rb
+++ spec/models/shop/shopping_cart_spec.rb
@@ -11,24 +11,52 @@ describe Shop::ShoppingCart do
 
     it "should empty an non-empty cart" do
       cart = Shop::ShoppingCart.create!
-      cart.add Shop::Product.create!(:name => 'Test 42', :price => 2.3)
+      cart.add FactoryGirl.build(:product, price: Money.new(230), name: 'Test 22')
       cart.size.should be 1
       cart.empty
       cart.size.should be 0
     end
   end
 
-  describe "item manipulation" do
-    it "should have 1 item after adding 1 item" do
+  describe "cart size" do
+    it "should have size 0" do
       cart = Shop::ShoppingCart.create!
-      cart.add Shop::Product.create!(name: 'Test 23', price: 4.2)
+      cart.size.should be 0
+    end
+
+    it "should have size 1 after adding product" do
+      cart = Shop::ShoppingCart.create!
+      prod = FactoryGirl.create(:product, price: Money.new(420), name: 'Test 19')
+      cart.add prod
       cart.size.should be 1
     end
 
-    it "should have 1 less item after item quantitty goes to 0" do
+    it "should have size 2 after adding products" do
       cart = Shop::ShoppingCart.create!
-      cart.add Shop::Product.create!(name: 'Test 23', price: 4.2)
-      cart.add Shop::Product.create!(name: 'Test 24', price: 4.2)
+      cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 20')
+      cart.size.should == 1
+      cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 21')
+      cart.size.should == 2
+    end
+
+    it "should have size 2 after incrementing product" do
+      cart = Shop::ShoppingCart.create!
+      prod = FactoryGirl.create(:product, price: Money.new(420), name: 'Test 22')
+      cart.add prod
+      cart.size.should be 1
+      cart.items.first.inc(:quantity, 1)
+      cart.size.should be 2
+    end
+
+  end
+
+  describe "item manipulation" do
+
+    it "should have 1 less item after quantity 0" do
+      cart = Shop::ShoppingCart.create!
+      cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 23')
+      cart.size.should be 1
+      cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 24')
       cart.size.should be 2
       cart.items.first.quantity = 0
       cart.size.should be 1
@@ -36,10 +64,34 @@ describe Shop::ShoppingCart do
 
     it "should have 2 items after incrementation" do
       cart = Shop::ShoppingCart.create!
-      cart.add Shop::Product.create!(name: 'Test 25', price: 4.2)
+      cart.add FactoryGirl.build(:product, price: Money.new(420), name: 'Test 25')
       cart.size.should be 1
       cart.items.first.quantity = 2
       cart.size.should be 2
     end
   end
+
+  describe "adding to cart" do
+    it "should increment quantity if product already in cart w/o options" do
+      cart = Shop::ShoppingCart.create!
+      prod = FactoryGirl.create(:product, price: Money.new(420), name: 'Test 26')
+      cart.add prod
+      cart.items.first.quantity.should == 1
+      cart.add prod
+      cart = Shop::ShoppingCart.find(cart) # Reload cart
+      cart.items.first.quantity.should == 2
+    end
+
+    it "should increment quantity if product already in cart w/ options" do
+      cart = Shop::ShoppingCart.create!
+      prod = FactoryGirl.create(:product_with_options, price: Money.new(420), name: 'Test 27')
+      opt_hash = {prod.options.first.name => prod.options.first.values.first}
+      cart.add prod, opt_hash
+      cart.items.first.quantity.should == 1
+      cart.add prod, opt_hash
+      cart = Shop::ShoppingCart.find(cart) # Reload cart
+      cart.items.first.quantity.should == 2
+    end
+  end
+
 end
