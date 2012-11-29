2c70c5415fb8eeaa4bf36c7c09bfc78f9611084d
Timo Sand <timo.j.sand@gmail.com>
2012-11-29 03:09:16 +0000

    CHANGED: Better Option specs

--- spec/models/shop/option_spec.rb
+++ spec/models/shop/option_spec.rb
@@ -1,25 +1,29 @@
 require 'spec_helper'
 
 describe Shop::Option do
-  describe "valiation" do
+  describe "validation" do
     it "should not allow empty values array" do
-      empty_values = Shop::Option.new(:name => "Test")
+      empty_values = FactoryGirl.build(:option, :name => "Test", values: [])
       empty_values.should_not be_valid
     end
 
     it "should not allow empty names" do
-      empty_name = Shop::Option.new(:values => ["foo"])
+      empty_name = FactoryGirl.build(:option, name: nil, :values => ["foo"])
       empty_name.should_not be_valid
     end
 
     it "should not allow 1 long names" do
-      shortest_name = Shop::Option.new(:name => 'a', :values => ["foo"])
+      shortest_name = FactoryGirl.build(:option, :name => 'a', :values => ["foo"])
       shortest_name.should_not be_valid
     end
 
     it "should not allow 2 long names" do
-      short_name = Shop::Option.new(:name => 'ab', :values => ["foo"])
+      short_name = FactoryGirl.build(:option, :name => 'ab', :values => ["foo"])
       short_name.should_not be_valid
+    end
+    it "should allow creation" do
+      opt = FactoryGirl.create(:option)
+      opt.should be_valid
     end
   end
 end
