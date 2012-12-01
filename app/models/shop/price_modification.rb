b5fc732dc4a5d8e5e5e7e9b62c8188b3a60e06ac
Timo Sand <timo.j.sand@gmail.com>
2012-12-01 20:36:07 +0000

    CHANGED: Added methods for calculating any percentage from the mod value

--- app/models/shop/price_modification.rb
+++ app/models/shop/price_modification.rb
@@ -5,7 +5,11 @@ class Shop::PriceModification
   field :name, :type => String
   field :value, :type => Float
 
-  def percentage
+  def add_percentage
     (self.value+100)/100
+  end
+
+  def substract_percentage
+    (100-self.value)/100
   end
 end
