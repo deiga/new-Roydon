f636ddd61322174fc8a75b40297ac55e3df511b2
Timo Sand <timo.j.sand@gmail.com>
2012-12-06 19:41:13 +0000

    CHANGED: Added factories for orders with items

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -9,5 +9,17 @@ FactoryGirl.define do
     postal_number "00100"
     payment "Cash"
     phone "0445566505"
+
+    factory :order_with_items do
+      items {
+        [6500, 12300, 540, 44321].inject([]) { |items, price| items << FactoryGirl.build(:order_item, product_price: Money.new(price))}
+      }
+    end
+  end
+
+  factory :order_item, class: Shop::OrderItem do
+    product_name 'Test product'
+    product_id Moped::BSON::ObjectId.new
+    tax 1.23
   end
 end
