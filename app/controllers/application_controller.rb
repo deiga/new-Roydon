bfd5a4c773da3cb6ad3a4a1adb59a759a4efd288
Timo Sand <timo.j.sand@gmail.com>
2012-12-06 19:49:32 +0000

    FIXED: Deprecation warning from upgrade to Rails 3.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,6 @@ class ApplicationController < ActionController::Base
           logger.info "Rendering 404: #{exception.message}"
       end
 
-      render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+      render :file => "#{Rails.root}/public/404", :formats => [:html], :status => 404, :layout => false
     end
 end
