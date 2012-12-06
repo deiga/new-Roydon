13486e8cfffd545e8f0efe59df1662b3f4027dbf
Timo Sand <timo.j.sand@gmail.com>
2012-12-06 19:39:55 +0000

    REMOVED: Useless print

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -17,7 +17,6 @@ class ShowsController < ApplicationController
       @upcoming_shows = Show.active.where( :date.gte => queried_year,
         :date.lt => queried_year.next_year )
     else
-      p "Current years"
       @upcoming_shows = Show.active.where( :date.gte => today )
       @earlier_shows = Show.active.where( :date.lt => today,
         :date.gte => Date.new(today.year) )
