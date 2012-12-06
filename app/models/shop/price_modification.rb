f295e1802d4c81fa31b014807d270de8afb3b9cc
Timo Sand <timo.j.sand@gmail.com>
2012-12-06 19:39:33 +0000

    ADDED: Tests for price modifications, validations

--- app/models/shop/price_modification.rb
+++ app/models/shop/price_modification.rb
@@ -3,7 +3,10 @@ class Shop::PriceModification
   include Mongoid::Timestamps
 
   field :name, :type => String
-  field :value, :type => Float
+  field :value, :type => Float, default: 0.0
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :value, numericality: true
 
   def add_percentage
     (self.value+100)/100
---
+++ spec/models/shop/price_modification_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Shop::PriceModification do
+
+  it "should create with default values" do
+    pm = Shop::PriceModification.create(name: 'Test1')
+    pm.should be_valid
+  end
+
+  it "should not be valid with no name" do
+    invalid_pm = Shop::PriceModification.create
+    invalid_pm.should_not be_valid
+  end
+
+  it "should not be valid with nonnumerical value" do
+    invalid_pm = Shop::PriceModification.create(name: 'Test2', value: 'foo')
+    invalid_pm.value.should == 0.0
+    invalid_pm.should be_valid
+  end
+
+  describe "percentage values" do
+    it "should be correct addition" do
+      pm = Shop::PriceModification.create(name: 'Test3', value: 23.0)
+      pm.add_percentage.should == 1.23
+    end
+    it "should be correct subtraction" do
+      pm = Shop::PriceModification.create(name: 'Test3', value: 23.0)
+      pm.subtract_percentage.should == 0.77
+    end
+  end
+end
