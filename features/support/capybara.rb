f7bf9a166bc9fae306a63b2f1eecca5254335027
Timo Sand <timo.j.sand@gmail.com>
2012-12-06 20:31:13 +0000

    CHANGED: Updated cucumber env.rb and moved custom requires to external files

---
+++ features/support/capybara.rb
@@ -0,0 +1,4 @@
+require 'capybara/rails'
+require 'capybara/cucumber'
+require 'capybara/session'
+#require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
