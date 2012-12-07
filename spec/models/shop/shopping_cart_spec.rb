7b9c049b615a9edacd92b2decc3f735994f27f6d
Timo Sand <timo.j.sand@gmail.com>
2012-12-07 00:07:58 +0000

    CHANGED: Added specs for price calculation. DRY'd code

--- spec/models/shop/shopping_cart_spec.rb
+++ spec/models/shop/shopping_cart_spec.rb
@@ -2,50 +2,51 @@ require 'spec_helper'
 
 describe Shop::ShoppingCart do
 
+  before(:each) do
+    @cart = Shop::ShoppingCart.create!
+  end
+
   describe "calling empty" do
     it "should do nothing on an empty cart" do
-      empty_shopping_cart = Shop::ShoppingCart.create!
-      empty_shopping_cart.empty
-      empty_shopping_cart.size.should be 0
+      @cart.empty
+      @cart.size.should be 0
     end
 
     it "should empty an non-empty cart" do
-      cart = Shop::ShoppingCart.create!
-      cart.add FactoryGirl.build(:product, price: Money.new(230), name: 'Test 22')
-      cart.size.should be 1
-      cart.empty
-      cart.size.should be 0
+      @cart.add FactoryGirl.build(:product, price: Money.new(230), name: 'Test 22')
+      @cart.size.should be 1
+      @cart.empty
+      @cart.size.should be 0
     end
   end
 
   describe "cart size" do
     it "should have size 0" do
-      cart = Shop::ShoppingCart.create!
-      cart.size.should be 0
+      @cart.size.should be 0
     end
 
     it "should have size 1 after adding product" do
-      cart = Shop::ShoppingCart.create!
       prod = FactoryGirl.create(:product, price: Money.new(420), name: 'Test 19')
-      cart.add prod
-      cart.size.should be 1
+      @cart.add prod
+      @cart.size.should be 1
     end
 
     it "should have size 2 after adding products" do
-      cart = Shop::ShoppingCart.create!
-      cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 20')
-      cart.size.should == 1
-      cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 21')
-      cart.size.should == 2
+      @cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 20')
+      @cart.size.should == 1
+      @cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 21')
+      @cart.size.should == 2
     end
 
     it "should have size 2 after incrementing product" do
-      cart = Shop::ShoppingCart.create!
       prod = FactoryGirl.create(:product, price: Money.new(420), name: 'Test 22')
-      cart.add prod
-      cart.size.should be 1
-      cart.items.first.inc(:quantity, 1)
-      cart.size.should be 2
+      lambda {
+        @cart.add prod
+      }.should change(@cart, :size).by(1)
+      lambda {
+        @cart.items.first.inc(:quantity, 1)
+      }.should change(@cart, :size).by(1)
+
     end
 
   end
@@ -53,44 +54,62 @@ describe Shop::ShoppingCart do
   describe "item manipulation" do
 
     it "should have 1 less item after quantity 0" do
-      cart = Shop::ShoppingCart.create!
-      cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 23')
-      cart.size.should be 1
-      cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 24')
-      cart.size.should be 2
-      cart.items.first.quantity = 0
-      cart.size.should be 1
+
+      lambda {
+        @cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 23')
+      }.should change(@cart, :size).by(1)
+      lambda {
+        @cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 24')
+      }.should change(@cart, :size).by(1)
+      @cart.items.first.quantity = 0
+      @cart.size.should be 1
     end
 
     it "should have 2 items after incrementation" do
-      cart = Shop::ShoppingCart.create!
-      cart.add FactoryGirl.build(:product, price: Money.new(420), name: 'Test 25')
-      cart.size.should be 1
-      cart.items.first.quantity = 2
-      cart.size.should be 2
+      lambda {
+        @cart.add FactoryGirl.create(:product, price: Money.new(420), name: 'Test 25')
+      }.should change(@cart, :size).by(1)
+      @cart.items.first.quantity = 2
+      @cart.size.should be 2
     end
   end
 
   describe "adding to cart" do
     it "should increment quantity if product already in cart w/o options" do
-      cart = Shop::ShoppingCart.create!
       prod = FactoryGirl.create(:product, price: Money.new(420), name: 'Test 26')
-      cart.add prod
-      cart.items.first.quantity.should == 1
-      cart.add prod
-      cart = Shop::ShoppingCart.find(cart) # Reload cart
-      cart.items.first.quantity.should == 2
+      @cart.add prod
+      @cart.items.first.quantity.should == 1
+      @cart.add prod
+      @cart.reload.items.first.quantity.should == 2
     end
 
     it "should increment quantity if product already in cart w/ options" do
-      cart = Shop::ShoppingCart.create!
       prod = FactoryGirl.create(:product_with_options, price: Money.new(420), name: 'Test 27')
       opt_hash = {prod.options.first.name => prod.options.first.values.first}
-      cart.add prod, opt_hash
-      cart.items.first.quantity.should == 1
-      cart.add prod, opt_hash
-      cart = Shop::ShoppingCart.find(cart) # Reload cart
-      cart.items.first.quantity.should == 2
+      @cart.add prod, opt_hash
+      @cart.items.first.quantity.should == 1
+      @cart.add prod, opt_hash
+      @cart.reload.items.first.quantity.should == 2
+    end
+  end
+
+  describe "price calculation" do
+    it "should have price 0 when empty" do
+      @cart.should be_empty
+      @cart.price.should eq Money.new(0)
+    end
+
+    it "should have correct price after adding product" do
+      prod = FactoryGirl.create(:product_with_options, price: Money.new(420), name: 'Test 28')
+      @cart.add prod
+      @cart.price.should eq prod.price
+    end
+
+    it "should have correct price after adding 2 products" do
+      prod = FactoryGirl.create(:product_with_options, price: Money.new(420), name: 'Test 28')
+      @cart.add prod
+      @cart.add prod
+      @cart.reload.price.should eq prod.price*2
     end
   end
 
