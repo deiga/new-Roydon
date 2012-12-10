18fe291266e203f63a359ea12254224f7222ba88
Timo Sand <timo.j.sand@gmail.com>
2012-12-10 13:11:41 +0000

    CHANGED: Working on feature correction

--- features/step_definitions/shopping_cart_steps.rb
+++ features/step_definitions/shopping_cart_steps.rb
@@ -1,11 +1,9 @@
 Given /^the following products are in the shopping cart:$/ do |table|
   # table is a Cucumber::Ast::Table
-  @shopping_cart ||= Shop::ShoppingCart.new
+  @shopping_cart ||= Shop::ShoppingCart.find_or_create_by
+  p @shopping_cart.inspect
+  p Shop::ShoppingCart.count
   table.hashes.each do |product|
     @shopping_cart.add Shop::Product.create!(product)
   end
-end
-
-Given /^the shopping cart is empty$/ do
-  pending # express the regexp above with the code you wish you had
 end
