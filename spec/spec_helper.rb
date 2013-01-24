4524f163ff4c4c78250466573c3610c27d191eda
Timo Sand <timo.j.sand@gmail.com>
2013-01-24 18:53:28 +0000

    CHANGED: Updated Rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ include ActionDispatch::TestProcess
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # == Mock Framework
+  # ## Mock Framework
   #
   config.mock_with :rspec
 
@@ -19,6 +19,12 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
 
   # Clean up the database
   require 'database_cleaner'
