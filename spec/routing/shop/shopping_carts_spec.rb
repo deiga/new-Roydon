2461870a7bf61a4037fe7956e0347612b1a4edf0
Timo Sand <timo.j.sand@gmail.com>
2013-01-26 16:05:37 +0000

    REFACTORED

--- spec/routing/shop/shopping_carts_spec.rb
+++ spec/routing/shop/shopping_carts_spec.rb
@@ -1,16 +1,25 @@
 require 'spec_helper'
 
 describe "routes for Shopping Carts" do
-  it "routes shop_shopping_cart_add_item_path to shopping carts controller" do
-    cart = Shop::ShoppingCart.new
-    product = FactoryGirl.build(:product, name: 'Test 1', price: '2.34')
-    expect(post: shop_shopping_cart_add_item_path(cart.id, product.id)).
-      to route_to("shop/shopping_carts#add_item", shopping_cart_id: cart.id.to_s, id: product.id.to_s)
+
+  before(:each) do
+    @cart = Shop::ShoppingCart.create!
   end
-  it "routes shop_shopping_cart_remove_item_path to shopping carts controller" do
-    cart = Shop::ShoppingCart.new
+
+  after(:each) do
+    @cart.destroy
+  end
+
+  it "routes shop_shopping_cart_add_item_path to shopping carts controller" do
+
     product = FactoryGirl.build(:product, name: 'Test 1', price: '2.34')
-    expect(post: shop_shopping_cart_remove_item_path(cart.id, product.id)).
-      to route_to("shop/shopping_carts#remove_item", shopping_cart_id: cart.id.to_s, id: product.id.to_s)
+    expect(post: shop_shopping_cart_add_item_path(@cart.id, product.id)).
+      to route_to("shop/shopping_carts#add_item", shopping_cart_id: @cart.id.to_s, id: product.id.to_s)
+  end
+
+  it "routes shop_shopping_cart_remove_item_path to shopping carts controller" do
+    product = FactoryGirl.build(:product, name: 'Test 1', price: '2.34')
+    expect(post: shop_shopping_cart_remove_item_path(@cart.id, product.id)).
+      to route_to("shop/shopping_carts#remove_item", shopping_cart_id: @cart.id.to_s, id: product.id.to_s)
   end
 end
