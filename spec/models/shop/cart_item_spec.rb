cf8800398235996c1e83bf5869b8f4187aa7a745
Timo Sand <timo.j.sand@gmail.com>
2013-01-26 16:05:00 +0000

    CHANGED: Added specs for cart item behaviour

--- spec/models/shop/cart_item_spec.rb
+++ spec/models/shop/cart_item_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Shop::CartItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @item = Shop::CartItem.new
+  end
+
+  after(:each) do
+    @item.destroy
+  end
+
+  it "should delete item when quantity 0" do
+    prod = FactoryGirl.create(:product, name: 'Test 1', price: 4.2)
+    p prod.inspect
+    p @item.inspect
+    @item.product = prod
+    # @item.should be_valid
+    p @item.inspect
+    @item.save
+    p @item.errors
+    @item.quantity = 0
+    @item.save
+    @item.should be_destroyed
+    prod.destroy
+  end
+
+  specify { @item.should_not be_valid }
 end
