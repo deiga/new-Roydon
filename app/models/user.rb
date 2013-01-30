ef75787e38fa0decba4cb20b895c5ab853a1f280
Timo Sand <timo.j.sand@gmail.com>
2013-01-30 21:30:10 +0000

    CHANGED: Refactored to better syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,34 +6,34 @@ class User
          :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
-  field :email,              :type => String
-  field :encrypted_password, :type => String, :default => ""
+  field :email,              type: String
+  field :encrypted_password, type: String, :default => ""
 
   validates :email, presence: true
 
   ## Recoverable
-  field :reset_password_token,   :type => String
-  field :reset_password_sent_at, :type => Time
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
 
   ## Rememberable
-  field :remember_created_at, :type => Time
+  field :remember_created_at, type: Time
 
   ## Trackable
-  field :sign_in_count,      :type => Integer, :default => 0
-  field :current_sign_in_at, :type => Time
-  field :last_sign_in_at,    :type => Time
-  field :current_sign_in_ip, :type => String
-  field :last_sign_in_ip,    :type => String
+  field :sign_in_count,      type: Integer, :default => 0
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
 
   ## Name
-  field :first_name,  :type => String
-  field :last_name,   :type => String
+  field :first_name,  type: String
+  field :last_name,   type: String
 
   ## Relations
   has_and_belongs_to_many :user_groups, class_name: 'Shop::UserGroup'
 
   ## Encryptable
-  # field :password_salt, :type => String
+  # field :password_salt, type: String
 
   ## Confirmable
   # field :confirmation_token,   :type => String
@@ -42,12 +42,12 @@ class User
   # field :unconfirmed_email,    :type => String # Only if using reconfirmable
 
   ## Lockable
-  # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
-  # field :locked_at,       :type => Time
+  # field :failed_attempts, type: Integer, :default => 0 # Only if lock strategy is :failed_attempts
+  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
+  # field :locked_at,       type: Time
 
   ## Token authenticatable
-  # field :authentication_token, :type => String
+  # field :authentication_token, type: String
 
   def is_admin?
     user_groups.map(&:name).include?('admin')
