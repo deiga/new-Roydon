f292e2bb92e14cb32dceb7cca945a3c621a0baf0
Timo Sand <timo.j.sand@gmail.com>
2013-02-24 18:45:56 +0000

    Fixed failure in price modification spec and refactored

--- spec/models/shop/price_modification_spec.rb
+++ spec/models/shop/price_modification_spec.rb
@@ -3,29 +3,29 @@ require 'spec_helper'
 describe Shop::PriceModification do
 
   it "should create with default values" do
-    pm = Shop::PriceModification.create!(name: 'Test1')
-    pm.should be_valid
+    subject.name = 'Test1'
+    subject.should be_valid
   end
 
   it "should not be valid with no name" do
-    invalid_pm = Shop::PriceModification.new
-    invalid_pm.should_not be_valid
+    subject.should_not be_valid
   end
 
   it "should not be valid with nonnumerical value" do
-    invalid_pm = Shop::PriceModification.create!(name: 'Test2', value: 'foo')
-    invalid_pm.value.should == 0.0
-    invalid_pm.should be_valid
+    invalid_pm = Shop::PriceModification.new(name: 'Test2', value: 'foo')
+    invalid_pm.should_not be_valid
   end
 
   describe "percentage values" do
     it "should be correct addition" do
-      pm = Shop::PriceModification.create!(name: 'Test3', value: 23.0)
-      pm.add_percentage.should == 1.23
+      subject.name = 'Test3'
+      subject.value = 23.0
+      subject.add_percentage.should == 1.23
     end
     it "should be correct subtraction" do
-      pm = Shop::PriceModification.create!(name: 'Test3', value: 23.0)
-      pm.subtract_percentage.should == 0.77
+      subject.name = 'Test4'
+      subject.value = 23.0
+      subject.subtract_percentage.should == 0.77
     end
   end
 end
