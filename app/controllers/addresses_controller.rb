1039ad19783ac3e4302adbba14f91590dc22666e
Timo Sand <timo.j.sand@gmail.com>
2013-03-05 13:43:58 +0000

    Added creation of addresses

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,11 +1,28 @@
 class AddressesController < ApplicationController
+
+  respond_to :html, :js
+
   def new
+    @address = Address.new
+
+    respond_with @address
   end
 
   def show
   end
 
   def create
+    @address = Address.new(address_params)
+    respond_to do |format|
+      if @address.save
+        current_user.addresses << @address
+        format.html { redirect_to users_show_path, notice: 'Address created' }
+        format.json { render json: @address, status: :created, location: @address }
+      else
+        format.html { render :new }
+        format.json { render json: @address.errors, status: 'Failure' }
+      end
+    end
   end
 
   def edit
@@ -16,4 +33,14 @@ class AddressesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def address_params
+      begin
+        params.require(:address).permit(:name, :street, :country, :city, :postal_number, :phone_number, :description, :home)
+      rescue ActionController::ParameterMissing
+        nil
+      end
+    end
 end
