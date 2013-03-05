5a54ba71e9c5cda1196bab38cd128a25306908ed
Timo Sand <timo.j.sand@gmail.com>
2013-03-05 14:14:44 +0000

    Refactored group type validation. Need to figure out a better way

--- app/models/shop/user_group.rb
+++ app/models/shop/user_group.rb
@@ -2,11 +2,11 @@ class Shop::UserGroup
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  GROUP_TYPES = ['admin', 'breeder', 'groomer']
+  # GROUP_TYPES = ['admin', 'breeder', 'groomer'] # TODO refactor these types somewhere else..
 
   field :name, :type => String
 
-  validates :name, presence: true, inclusion: GROUP_TYPES, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   has_and_belongs_to_many :users
   has_many :discounts, class_name: 'Shop::Discount'
