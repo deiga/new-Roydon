8b5f2bc844d837dfd526875c46b00cff8f164b6c
Timo Sand <timo.j.sand@gmail.com>
2013-03-05 10:18:49 +0000

    Refactoring specs

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,51 +2,57 @@ require 'spec_helper'
 
 describe Address do
 
-  subject { FactoryGirl.build(:address) }
+  subject(:valid) { FactoryGirl.build(:address) }
 
-  it { should be_valid }
+  it { valid.should be_valid }
 
   describe "Phone number validations" do
 
-    it "should not be valid with empty phone number" do
-      subject.phone_number = ""
-      subject.should_not be_valid
+    subject(:no_phone) { FactoryGirl.build(:address, phone_number: number) }
+
+    context "empty number" do
+      let(:number) { "" }
+      it { should_not be_valid }
     end
 
-    it "should not be valid with short phone number" do
-      subject.phone_number = "12345"
-      subject.should_not be_valid
+    context "short phone number" do
+      let(:number) { "12345" }
+      it { should_not be_valid }
     end
 
-    it "should be valid with valid number" do
-      subject.should be_valid
+    context "valid number" do
+      let(:number) { "0445655606" }
+      it { should be_valid }
     end
 
-    it "should be valid with long-form valid number" do
-      subject.phone_number = "00358445655606"
-      subject.should be_valid
+    context "long-form valid number" do
+      let(:number) { "00358445655606" }
+      it { should be_valid }
     end
 
-    it "should be valid with valid landline number" do
-      subject.phone_number = "09123456"
-      subject.should be_valid
+    context "valid landline number" do
+      let(:number) { "09123456" }
+      it { should be_valid }
     end
-    it "should not be valid with german prefix number" do
-      subject.phone_number = "+49123456"
-      subject.should_not be_valid
+
+    context "german prefix" do
+      let(:number) { "+49123456" }
+      it { should_not be_valid }
     end
+
   end
 
   describe "Before filters" do
     it "should set name if empty" do
-      subject.save
-      subject.name.should eq subject.street
+      valid.name.should be_blank
+      valid.save
+      valid.name.should eq valid.street
     end
 
     it "should leave name if set" do
-      subject.name = 'Foo'
-      subject.save
-      subject.name.should eq 'Foo'
+      valid.name = 'Foo'
+      valid.save
+      valid.name.should eq 'Foo'
     end
   end
 end
