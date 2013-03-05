8d1694d4bedeb2f51caf52b61f9e71487852abf6
Timo Sand <timo.j.sand@gmail.com>
2013-03-05 16:52:48 +0000

    Added user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe User do
+
+  subject { FactoryGirl.build(:user) }
+
+  it { should be_valid }
+
+  describe "dependencies" do
+    it "should add addresses" do
+      subject.addresses.should be_empty
+      subject.addresses << FactoryGirl.build(:address)
+      subject.addresses.should_not be_empty
+    end
+
+    it "should add orders" do
+      subject.orders.should be_empty
+      subject.orders << FactoryGirl.build(:order)
+      subject.orders.should_not be_empty
+    end
+  end
+
+end
