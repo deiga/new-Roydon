81a3d31cbb57951249b650457e77f54374267edf
Timo Sand <timo.j.sand@gmail.com>
2013-03-11 02:22:12 +0000

    Refined remote dev environment to be a better mix of staging and dev

--- config/environments/development_remote.rb
+++ config/environments/development_remote.rb
@@ -1,23 +1,24 @@
 Roydon::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
-
-  # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=2592000"
 
-  config.log_level = :debug
-
-  # config.logger.level = Logger.const_get(
-  #   ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
-  # )
-
+  config.assets.compress = false
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :yui
+  config.assets.compile = false
+  config.assets.digest = true
   config.assets.precompile += %w( polyfills.js )
+  config.assets.debug = true
 
-  # Show full error reports and disable caching
+  config.whiny_nils = true
+  config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(
+    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
+  )
+
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
@@ -34,12 +35,6 @@ Roydon::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
 
   # S3 config for paperclip
   config.paperclip_defaults = {:storage => :s3, :s3_credentials => {access_key_id: ENV['S3_ACCESS'], secret_access_key: ENV['S3_SECRET']}, bucket: "roydon-dev", path: ":attachment/:id/:style.:extension", url: ":s3_domain_url"}
