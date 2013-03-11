5fdd81c2c4cd8747f856f3b1b7668afa739f0c3e
Timo Sand <timo.j.sand@gmail.com>
2013-03-11 02:22:30 +0000

    Removed useless lines

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,22 +14,3 @@ preload_app true
 # Immediately restart any workers that
 # haven't responded within 30 seconds
 timeout 30
-
-before_fork do |server, worker|
-  # Replace with MongoDB or whatever
-  # if defined?(ActiveRecord::Base)
-  #   ActiveRecord::Base.connection.disconnect!
-  #   Rails.logger.info('Disconnected from ActiveRecord')
-  # end
-
-  # sleep 1
-end
-
-after_fork do |server, worker|
-  # Replace with MongoDB or whatever
-  # if defined?(ActiveRecord::Base)
-  #   ActiveRecord::Base.establish_connection
-  #   Rails.logger.info('Connected to ActiveRecord')
-  # end
-
-end
