08aae5738967fa3544e5efc03e929e9d7132bee3
Timo Sand <timo.j.sand@gmail.com>
2013-03-16 14:12:52 +0000

    Refactored product specs to use shorter notation

--- spec/models/shop/product_spec.rb
+++ spec/models/shop/product_spec.rb
@@ -1,37 +1,28 @@
 require 'spec_helper'
 
 describe Shop::Product do
+  subject { FactoryGirl.build(:product) }
+  let(:no_name) { FactoryGirl.build(:product, :name => '') }
+  let(:negative_price) { FactoryGirl.build(:product, price: Money.new(-550)) }
+  let(:with_options) { FactoryGirl.create(:product_with_options) }
 
-  it "should create a new Shop::Product given valid attributes" do
-    created = FactoryGirl.build(:product)
-    created.should be_valid
-  end
+  it { should be_valid }
 
-  it "should be empty" do
-    all_products = Shop::Product.all
-    all_products.should be_empty
-  end
+  specify { subject.options.should be_empty}
 
   it "should not accept zero length name" do
-    no_name_product = FactoryGirl.build(:product, :name => '')
-    no_name_product.should_not be_valid
+    no_name.should_not be_valid
   end
 
   it "should have a name" do
-    no_name_product = Shop::Product.new
-    no_name_product.should_not be_valid
+    Shop::Product.new.should_not be_valid
   end
 
   it "should not have negative price" do
-    negative_price_product = FactoryGirl.build(:product, price: Money.new(-550))
-    negative_price_product.should_not be_valid
+    negative_price.should_not be_valid
   end
 
-
-  it "should have no categories" do
-    no_categories_product = FactoryGirl.build(:product)
-    no_categories_product.categories.should be_empty
-  end
+  specify { subject.categories.should be_empty }
 
   it "should normalize filename" do
     Shop::Product.any_instance.stub(:save_attached_files => true)
@@ -39,24 +30,15 @@ describe Shop::Product do
     FactoryGirl.create(:product_with_image).image_file_name.should eql("test_image_1.jpg")
   end
 
-  it "should create prodcut with options" do
-    prod = FactoryGirl.create(:product_with_options)
-    prod.options.should_not be_empty
-  end
+  specify { with_options.options.should_not be_empty }
 
-  it "should have correct options" do
-    prod = FactoryGirl.create(:product_with_options)
-    prod.options.should_not be_empty
-    prod.options.first.values.should include('Red')
-  end
+  specify { with_options.options.first.values.should include('Red') }
 
-  it "should return unmodified price when no discount" do
-    subject.discount.should_not be_present
-    subject.discounted_price.should eq subject.price
-  end
+  specify { subject.discount.should_not be_present}
+
+  specify { subject.discounted_price.should eq subject.price }
 
   describe "discounted products" do
-    subject { FactoryGirl.build(:product) }
     it "should return discounted price" do
       discount = FactoryGirl.create(:discount, value: 10)
       subject.discount = discount
