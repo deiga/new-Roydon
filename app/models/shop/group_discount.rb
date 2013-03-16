6edc3cf47840583bd705fcbe67dfc006465519c8
Timo Sand <timo.j.sand@gmail.com>
2013-03-16 14:13:21 +0000

    Recreated group discount model

--- app/models/shop/group_discount.rb
+++ app/models/shop/group_discount.rb
@@ -1,5 +1,13 @@
-class Shop::GroupDiscount < Shop::Discount
-  field :amount_needed, :type => Integer
+class Shop::GroupDiscount
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include ActiveModel::ForbiddenAttributesProtection
 
-  belongs_to :user
+  has_and_belongs_to_many :products, class_name: 'Shop::Product', inverse_of: nil
+  has_many :categories, class_name: 'Shop::Category'
+
+  field :name, type: String
+  field :scheme, type: Hash
+
+  validates :name, presence: true
 end
