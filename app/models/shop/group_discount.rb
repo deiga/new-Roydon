bccae86bb5b679ee1df1c182e26ecfa6b21d3d96
Timo Sand <timo.j.sand@gmail.com>
2013-03-17 18:27:42 +0000

    Green step, group discount returns price modification

--- app/models/shop/group_discount.rb
+++ app/models/shop/group_discount.rb
@@ -9,5 +9,32 @@ class Shop::GroupDiscount
   field :name, type: String
   field :scheme, type: Hash
 
-  validates :name, presence: true
+  validates :name, :scheme, presence: true
+
+  def apply_price(cart)
+    cart_products = cart.items.reduce([]) { |memo, item| (memo << item.product) * item.quantity }
+    cart_products.select! {|item| products.include?(item)}
+    prod_amount = cart_products.size
+    if prod_amount >= scheme.keys.sort.first
+      old_price = cart_products.reduce(Money.new(0)) { |sum, prod| sum += prod.price }
+      unless prod_amount >= scheme.keys.max
+        values = scheme.keys.include?(prod_amount) ? [prod_amount] : scheme.keys.select { |key| key < prod_amount }
+      else
+        values = scheme.keys
+      end
+      new_price = Money.new(0)
+      begin
+        value = values.max
+        puts "#{scheme[value]}, #{value}, #{scheme[value][:cents]}"
+        new_price += Money.new(scheme[value][:cents])
+        prod_amount -= value
+        values.delete(value) unless prod_amount >= value
+      end while values.present?
+      old_price -= cart_products.first.price * prod_amount
+      [new_price, old_price]
+    else
+      nil
+    end
+  end
+
 end
