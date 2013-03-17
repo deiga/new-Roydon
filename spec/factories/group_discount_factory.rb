31bd985ae36a7236db915464b6acfbd52ab1ad01
Timo Sand <timo.j.sand@gmail.com>
2013-03-17 17:44:14 +0000

    Added factory for discount schemes

--- spec/factories/group_discount_factory.rb
+++ spec/factories/group_discount_factory.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :group_discount, class: Shop::GroupDiscount  do
     name "Test group discount"
-    scheme {}
+
+    factory :group_discount_with_scheme do
+      scheme {{5 => Money.new(1000)}}
+    end
   end
 end
