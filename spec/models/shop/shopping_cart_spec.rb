65e66acb0b2e2d87a224564b234b32ccae32b935
Timo Sand <timo.j.sand@gmail.com>
2013-03-20 20:35:57 +0000

    Refactored to use names subject

--- spec/models/shop/shopping_cart_spec.rb
+++ spec/models/shop/shopping_cart_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Shop::ShoppingCart do
 
-  subject { FactoryGirl.create(:cart) }
+  subject(:cart) { FactoryGirl.create(:cart) }
   let(:product) { FactoryGirl.build(:product, price: Money.new(230), name: 'Test 1') }
   let(:product2) { FactoryGirl.build(:product, price: Money.new(420), name: 'Test 2') }
   let(:product_with_options) { FactoryGirl.build(:product_with_options, price: Money.new(420), name: 'Test 3') }
@@ -11,74 +11,74 @@ describe Shop::ShoppingCart do
   it { should be_empty }
 
   it "should empty an non-empty cart" do
-    subject.add product
-    subject.size.should eq 1
+    cart.add product
+    cart.size.should eq 1
   end
 
   describe "cart size" do
     it "should have size 2 after adding products" do
-      subject.add product
-      subject.add product2
-      subject.size.should eq 2
+      cart.add product
+      cart.add product2
+      cart.size.should eq 2
     end
 
     it "should have size 2 after incrementing product" do
-      subject.add product
-      subject.add product
-      subject.size.should eq 2
-      subject.items.count.should eq 1
+      cart.add product
+      cart.add product
+      cart.size.should eq 2
+      cart.items.count.should eq 1
     end
   end
 
   describe "item manipulation" do
 
     it "should have 1 less item after quantity 0" do
-      subject.add product
-      subject.add product2
-      subject.items.first.quantity = 0
-      subject.size.should be 1
+      cart.add product
+      cart.add product2
+      cart.items.first.quantity = 0
+      cart.size.should be 1
     end
 
     it "should have 2 items after incrementation" do
-      subject.add product
-      subject.items.first.quantity = 2
-      subject.size.should be 2
+      cart.add product
+      cart.items.first.quantity = 2
+      cart.size.should be 2
     end
 
     it "should have 2 items after incrementation" do
-      subject.add product
-      subject.items.first.inc(:quantity, 1)
-      subject.size.should be 2
+      cart.add product
+      cart.items.first.inc(:quantity, 1)
+      cart.size.should be 2
     end
   end
 
   describe "adding to cart" do
     it "should increment quantity if product already in cart w/o options" do
-      subject.add product
-      subject.add product
-      subject.items.first.quantity.should eq 2
+      cart.add product
+      cart.add product
+      cart.items.first.quantity.should eq 2
     end
 
     it "should increment quantity if product already in cart w/ options" do
       opt_hash = {product_with_options.options.first.name => product_with_options.options.first.values.first}
-      subject.add product_with_options, opt_hash
-      subject.add product_with_options, opt_hash
-      subject.items.first.quantity.should eq 2
+      cart.add product_with_options, opt_hash
+      cart.add product_with_options, opt_hash
+      cart.items.first.quantity.should eq 2
     end
   end
 
   describe "price calculation" do
-    it { subject.price.should eq Money.new(0) }
+    it { cart.price.should eq Money.new(0) }
 
     it "should have correct price after adding product" do
-      subject.add product
-      subject.price.should eq product.price
+      cart.add product
+      cart.price.should eq product.price
     end
 
     it "should have correct price after adding 2 products" do
-      subject.add product
-      subject.add product
-      subject.price.should eq product.price*2
+      cart.add product
+      cart.add product
+      cart.price.should eq product.price*2
     end
   end
 
