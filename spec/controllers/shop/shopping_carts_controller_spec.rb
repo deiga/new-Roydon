0a564175d516170e51b459d7b14dcd9dd752fdf0
Timo Sand <timo.j.sand@gmail.com>
2013-03-24 16:01:58 +0000

    Added specs for sad paths of add/remove

--- spec/controllers/shop/shopping_carts_controller_spec.rb
+++ spec/controllers/shop/shopping_carts_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Shop::ShoppingCartsController do
 
+  let(:product) { FactoryGirl.create(:product, :name => "Test product", price: 5.3) }
+
   describe "GET 'edit'" do
     it "returns http success" do
       get 'edit', :id => Shop::ShoppingCart.create!
@@ -26,25 +28,37 @@ describe Shop::ShoppingCartsController do
   describe "POST 'add_item'" do
 
     it "should have one product in cart" do
-      prod = FactoryGirl.create(:product, :name => "Test product", price: 5.3)
-      post :add_item, shopping_cart_id: Shop::ShoppingCart.create!, id: prod.id, options: {}
+      post :add_item, shopping_cart_id: Shop::ShoppingCart.create!, id: product.id, options: {}
       @cart = assigns(:cart)
       @cart.items.size.should be 1
-      prod.destroy
+    end
+
+    it "should fail to add item" do
+      post :add_item, shopping_cart_id: Shop::ShoppingCart.create!, id: product, options: {}
+      @cart = assigns(:cart)
+      @cart.should_receive(:add)
+      post :add_item, shopping_cart_id: @cart.id, id: product, options: {}
+      flash[:alert].should eq (I18n.t 'shop.cart.add.failure')
     end
   end
 
   describe "DELETE 'remove_item" do
     it "should remove item from cart" do
-      prod = FactoryGirl.create(:product, :name => "Test product", price: 5.3)
-      delete :add_item, shopping_cart_id: Shop::ShoppingCart.create!, id: prod, options: {}
+      post :add_item, shopping_cart_id: Shop::ShoppingCart.create!, id: product, options: {}
       @cart = assigns(:cart)
       @cart.items.size.should be 1
       delete :remove_item, shopping_cart_id: @cart.id, id: @cart.items.first.id
       @cart = assigns(:cart)
       @cart.items.should be_empty
       @cart.should be_empty
-      prod.destroy
+    end
+
+    it "should fail to remove item" do
+      post :add_item, shopping_cart_id: Shop::ShoppingCart.create!, id: product, options: {}
+      @cart = assigns(:cart)
+      @cart.should_receive(:remove_item)
+      delete :remove_item, shopping_cart_id: @cart.id, id: @cart.items.first.id
+      flash[:alert].should eq (I18n.t 'shop.cart.remove.failure')
     end
   end
 end
