703f93928592de2c0ef30ee088f50cb88ef3e018
Timo Sand <timo.j.sand@gmail.com>
2013-03-24 14:47:43 +0000

    Whitespace refactor

--- app/models/address.rb
+++ app/models/address.rb
@@ -21,8 +21,8 @@ class Address
   validates :street, presence: true, length: { minimum: 4}
   validates :postal_number, length: { is: 5}
   validates :phone_number, format: { with: /^((([\+][\s]{0,1})|([0]{2}[\s-]{0,1}))([358]{3})([\s-]{0,1})|([0]{1}))(([1-9]{1}[0-9]{0,1})([\s-]{0,1})([0-9]{2,4})([\s-]{0,1})([0-9]{2,4})([\s-]{0,1}))([0-9]{0,3}){1}$/}
-  private
 
+  private
     def set_name
       if name.blank?
         self.name = street
