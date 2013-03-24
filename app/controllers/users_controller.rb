a3eec913d195d116223da72992bf8a9449bf063e
Timo Sand <timo.j.sand@gmail.com>
2013-03-24 00:36:20 +0000

    Added caching to users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :authenticate_user!
+  caches_action :show, cache_path: proc {{ tag: User.find(params[:id]).updated_at.to_i }}
 
   def show
     @user = User.find(params[:id])
