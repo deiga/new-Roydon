8330ccdad7738f75fb33082d1ed3a481532df242
Timo Sand <timo.j.sand@gmail.com>
2013-03-24 10:03:37 +0000

    Caching of show action in orders controller

--- app/controllers/shop/orders_controller.rb
+++ app/controllers/shop/orders_controller.rb
@@ -1,5 +1,7 @@
 class Shop::OrdersController < Shop::ShopController
 
+  caches_action :show, cache_path: proc {{ tag: Shop::Order.find(order_id_param).updated_at.to_i }}
+
   respond_to :html, :js
 
   def new
