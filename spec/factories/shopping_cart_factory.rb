4a75d2a6f38ec7047a4ae89a2778b94b5d2fbc84
Timo Sand <timo.j.sand@gmail.com>
2013-04-08 17:24:44 +0100

    Removed duplicate factory and nested cart factory

--- spec/factories/shopping_cart_factory.rb
+++ spec/factories/shopping_cart_factory.rb
@@ -1,13 +1,8 @@
 FactoryGirl.define do
   factory :cart, class: Shop::ShoppingCart do
-  end
-
-  factory :cart_with_product, class: Shop::ShoppingCart do
-    items {[FactoryGirl.build(:cart_item)]}
-  end
-
-  factory :cart_item, class: Shop::CartItem do
-    product FactoryGirl.build(:product)
+    factory :cart_with_product do
+      items {[FactoryGirl.build(:cart_item)]}
+    end
   end
 
 end
