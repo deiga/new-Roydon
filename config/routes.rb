cb53903b66d224cd468987cae25a9928d2a3ff01
Timo Sand <timo.j.sand@gmail.com>
2013-04-09 16:08:09 +0100

    Removed unused code

--- config/routes.rb
+++ config/routes.rb
@@ -42,47 +42,6 @@ Roydon::Application.routes.draw do
 
   root to: 'pages#home'
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # See how all your routes lay out with "rake routes"
   match "/404" => 'errors#not_found'
   match "/500" => 'errors#server_error'
 
