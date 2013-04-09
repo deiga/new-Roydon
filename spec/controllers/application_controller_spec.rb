18bec457341b1fadd97cb3914f5fdeb174f6081a
Timo Sand <timo.j.sand@gmail.com>
2013-04-09 16:54:13 +0100

    Anonymous controller to test exception handling

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,16 +1,36 @@
 require 'spec_helper'
 
 describe ApplicationController do
+  controller do
+    def index
+      params.require(:a)
+    end
+  end
 
-  context "requests are not local" do
-    render_views
+  render_views
 
-    it { Rails.application.config.consider_all_requests_local.should be_false }
+  it { Rails.application.config.consider_all_requests_local.should be_false }
 
-    it "should render 505 on Exception"
+  it "should render 500 on Exception" do
+    controller.stub(:index).and_raise(Exception)
+    get :index
+    response.should redirect_to('/500')
+  end
 
-    it "should render 404 on unknow controller"
-    it "should render 404 on action not found"
-    it "should render 400 on parameter missing"
+  it "should render 404 on unknown controller" do
+    controller.stub(:index).and_raise(ActionController::UnknownController)
+    get :index
+    response.should redirect_to('/404')
+  end
+
+  it "should render 404 on action not found" do
+    controller.stub(:index).and_raise(AbstractController::ActionNotFound)
+    get :index
+    response.should redirect_to('/404')
+  end
+
+  it "should render 400 on parameter missing" do
+    get :index
+    response.should redirect_to('/400')
   end
 end
