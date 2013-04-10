cb4d983334e7ade81d72a68d1ef1551d28673016
Timo Sand <timo.j.sand@gmail.com>
2013-04-10 11:54:51 +0100

    Removed redundancy

--- features/support/capybara.rb
+++
@@ -1,4 +0,0 @@
-require 'capybara/rails'
-require 'capybara/cucumber'
-require 'capybara/session'
-#require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
