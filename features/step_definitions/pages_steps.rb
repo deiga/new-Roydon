01fc1daa6bfd53d47e2549b57b170751bd20a974
Timo Sand <timo.j.sand@gmail.com>
2013-09-30 14:57:23 +0300

    Added step definitions

--- features/step_definitions/pages_steps.rb
+++ features/step_definitions/pages_steps.rb
@@ -2,12 +2,20 @@ When(/^I am on the (.*) page$/) do |page_name|
   visit send(page_name.split(/\s+/).push('path').join('_').to_sym)
 end
 
+When(/^I follow "(.*?)"$/) do |link_name|
+  click_on(link_name)
+end
+
 Then(/^I should see "(.*?)" in the title$/) do |expected|
   page.should have_title(expected)
 end
 
-Then(/^I should see "(.*?)" in the selector "(.*?)"$/) do |expected, selector|
-  page.should have_selector selector, text: expected
+Then(/^I should see "(.*?)"(?: in the selector "(.*?)")?$/) do |expected, selector|
+  if selector
+    page.should have_selector selector, text: expected
+  else
+    page.should have_content expected
+  end
 end
 
 Then /^show me the page$/ do
--- features/step_definitions/shopping_cart_steps.rb
+++ features/step_definitions/shopping_cart_steps.rb
@@ -1,8 +1,14 @@
 Given /^the following products are in the cart:$/ do |table|
   # table is a Cucumber::Ast::Table
-  @cart ||= Shop::ShoppingCart.find_or_create_by
   table.hashes.each do |product|
-    @cart.add Shop::Product.create!(product)
+    within("#product-#{product['name'].parameterize}") do
+      click_on('Lisää ostoskoriin')
+    end
   end
-  @cart.size.should == table.hashes.size
+end
+
+When /^I add "(.*?)" to shopping cart$/ do |product_name|
+  within("#product-#{product_name.parameterize}") do
+    click_on('Lisää ostoskoriin')
+  end
 end
