16dcdf9777715b6edf56bd16925b5513a43894bf
Timo Sand <timo.j.sand@gmail.com>
2013-10-02 08:25:12 +0300

    Updated test

--- spec/helpers/shows_helper_spec.rb
+++ spec/helpers/shows_helper_spec.rb
@@ -2,20 +2,24 @@ require 'spec_helper'
 
 describe ShowsHelper do
 
-  after do
+  after(:each) do
     Timecop.return
   end
 
   it { display_show_year(2011).should be 2011 }
 
   it "should return year range, if month >= 9" do
-    Timecop.travel(Date.new(2012, 9, 1))
+    new_time = Date.new(2012, 9, 1)
+    Timecop.travel(new_time)
+    new_time.should eq Date.today
     display_show_year(2012).should == '2012 - 2013'
   end
 
   it "should display the current year" do
     year = Date.today.year
-    Timecop.travel(Date.new(year, 1, 1))
+    new_time = Date.new(year, 1, 1)
+    Timecop.travel(new_time)
+    new_time.should eq Date.today
     display_show_year.should be year
   end
 end
