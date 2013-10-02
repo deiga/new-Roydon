4e2a9e3f235d2a7207f58fd00671f980f6b0b00f
Timo Sand <timo.j.sand@gmail.com>
2013-10-02 08:50:05 +0300

    Changed to use time

--- spec/helpers/shows_helper_spec.rb
+++ spec/helpers/shows_helper_spec.rb
@@ -9,17 +9,17 @@ describe ShowsHelper do
   it { display_show_year(2011).should be 2011 }
 
   it "should return year range, if month >= 9" do
-    new_time = Date.new(2012, 9, 1)
-    Timecop.travel(new_time)
-    new_time.should eq Date.today
+    new_time = Time.new(2012, 9, 1)
+    Timecop.freeze(new_time)
+    new_time.should eq Time.now
     display_show_year(2012).should == '2012 - 2013'
   end
 
   it "should display the current year" do
     year = Date.today.year
-    new_time = Date.new(year, 1, 1)
-    Timecop.travel(new_time)
-    new_time.should eq Date.today
+    new_time = Time.new(year, 1, 1)
+    Timecop.freeze(new_time)
+    new_time.should eq Time.now
     display_show_year.should be year
   end
 end
