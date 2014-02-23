807a001fb9fd2cb0822c99729d8e490ee071c48c
Timo Sand <timo.j.sand@gmail.com>
2014-02-24 00:12:50 +0200

    Rewrote configs

--- config/application.rb
+++ config/application.rb
@@ -44,9 +44,6 @@ module Roydon
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :password_confirmation]
-
     # Enable the asset pipeline
     config.assets.enabled = true
 
