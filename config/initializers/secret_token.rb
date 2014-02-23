ef56feacf6e914b4e9a9afd86678232feb681226
Timo Sand <timo.j.sand@gmail.com>
2014-02-23 17:39:29 +0200

    Added SECRET_KEY_BASE

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Roydon::Application.config.secret_token = ENV['SECRET_TOKEN']
+Roydon::Application.config.secret_key_base = ENV['SECRET_TOKEN']
