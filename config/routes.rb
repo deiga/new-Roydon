a373f589264b0015ff3eb5d472e563828916a680
Timo Sand <timo.j.sand@gmail.com>
2014-03-11 22:51:09 +0200

    Extracted mall routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,20 +12,7 @@ Roydon::Application.routes.draw do
 
   resources :stories, only: [:index], path: :news, constraints: { id: BSON_ID_REGEX }
 
-  namespace :shop do
-    resources :products, only: [:index, :show], constraints: { id: BSON_ID_REGEX } do
-      collection do
-        get 'categories/:category', action: :index, as: 'category', constraints: { category: /[a-zA-Z0-9~-]+/i }
-      end
-    end
-    resources :shopping_carts, except: [:index], constraints: { id: BSON_ID_REGEX } do
-      delete '/remove_item/:id', action: 'remove_item', as: 'remove_item'
-      match '/add_item/:id', action: 'add_item', as: 'add_item', via: [:get, :post]
-    end
-    get '/' => 'shop#index'
-    get 'search' => 'shop#search'
-    resources :orders, except: [:index]
-  end
+  mount Mall::Engine, at: '/shop', as: 'shop'
 
   resources :shows, only: [], constraints: { year: /\d{4}/ } do
     collection do
