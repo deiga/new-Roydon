490b6380b96e61852cee27511c98d77671dac54c
Timo Sand <timo.j.sand@gmail.com>
2014-03-11 22:51:09 +0200

    Extracted mall routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,15 @@
 Mall::Engine.routes.draw do
+
+  resources :products, only: [:index, :show], constraints: { id: BSON_ID_REGEX } do
+    collection do
+      get 'categories/:category', action: :index, as: 'category', constraints: { category: /[a-zA-Z0-9~-]+/i }
+    end
+  end
+  resources :shopping_carts, except: [:index], constraints: { id: BSON_ID_REGEX } do
+    delete '/remove_item/:id', action: 'remove_item', as: 'remove_item'
+    match '/add_item/:id', action: 'add_item', as: 'add_item', via: [:get, :post]
+  end
+  get '/' => 'shop#index'
+  get 'search' => 'shop#search'
+  resources :orders, except: [:index]
 end
