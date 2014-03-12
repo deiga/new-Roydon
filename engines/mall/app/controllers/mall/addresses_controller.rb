2e6c99f7876c6bc109259f121deafb39da07ca4d
Timo Sand <timo.j.sand@gmail.com>
2014-03-12 17:12:15 +0200

    Fixed namespacing

--- engines/mall/app/controllers/mall/addresses_controller.rb
+++ engines/mall/app/controllers/mall/addresses_controller.rb
@@ -1,50 +1,51 @@
-class AddressesController < ApplicationController
+module Mall
+  class AddressesController < Mall::ApplicationController
 
-  respond_to :html, :js
+    respond_to :html, :js
 
-  def new
-    @address = Address.new
+    def new
+      @address = Address.new
 
-    respond_with @address
-  end
-
-  def show
-    @address = Address.find(address_id_params)
-    stale? @address do
       respond_with @address
     end
-  end
 
-  def create
-    @address = Address.new(address_params)
-    respond_to do |format|
-      if @address.save
-        current_user.addresses << @address
-        format.html do
-          set_flash :success, object: @address
-          redirect_to(user_path(current_user.id)) and return
-        end
-        format.json { render json: @address, status: :created, location: @address }
-      else
-        format.html do
-          set_flash :error, object: @address
-          render :new
-        end
-        format.json { render json: @address.errors, status: 'Failure' }
+    def show
+      @address = Address.find(address_id_params)
+      stale? @address do
+        respond_with @address
       end
     end
-  end
 
-  def edit
-  end
+    def create
+      @address = Address.new(address_params)
+      respond_to do |format|
+        if @address.save
+          current_user.addresses << @address
+          format.html do
+            set_flash :success, object: @address
+            redirect_to(user_path(current_user.id)) and return
+          end
+          format.json { render json: @address, status: :created, location: @address }
+        else
+          format.html do
+            set_flash :error, object: @address
+            render :new
+          end
+          format.json { render json: @address.errors, status: 'Failure' }
+        end
+      end
+    end
 
-  def update
-  end
+    def edit
+    end
 
-  def destroy
-  end
+    def update
+    end
 
-  private
+    def destroy
+    end
+
+    private
 
     def address_params
       begin
@@ -61,4 +62,5 @@ class AddressesController < ApplicationController
         nil
       end
     end
+  end
 end
