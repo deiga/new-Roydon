9119037460ad099d783456950428495a4fc661d5
Timo Sand <timo.j.sand@gmail.com>
2014-03-12 16:40:02 +0200

    Added admin model and updated seeds

--- app/models/mall/user.rb
+++ app/models/mall/user.rb
@@ -15,8 +15,6 @@ module Mall
     field :email,              type: String, default: ''
     field :encrypted_password, type: String, default: ''
 
-    validates :email, presence: true, uniqueness: true
-
     ## Recoverable
     field :reset_password_token,   type: String
     field :reset_password_sent_at, type: Time
@@ -53,10 +51,6 @@ module Mall
     # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
     # field :locked_at,       type: Time
 
-    def admin?
-      user_groups.map(&:name).include?('admin')
-    end
-
     def password_required?
       super if confirmed?
     end
@@ -74,11 +68,6 @@ module Mall
 
     def name
       "#{first_name} #{last_name}"
-    end
-
-    # Turn off trackable for admin users
-    def update_tracked_fields!(request)
-      super(request) unless admin?
     end
 
     rails_admin do
