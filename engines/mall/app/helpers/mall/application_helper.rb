6569184bfbf8f2fd6ea70eafd9c264ab54037559
Timo Sand <timo.j.sand@gmail.com>
2014-03-12 16:06:57 +0200

    Fixing mall namespace

--- engines/mall/app/helpers/mall/application_helper.rb
+++ engines/mall/app/helpers/mall/application_helper.rb
@@ -1,4 +1,7 @@
 module Mall
   module ApplicationHelper
+    def category_link(category)
+    link_to raw(category.name.gsub(/\s/, '&nbsp;')), category_shop_products_path(category), id: category.permalink
+  end
   end
 end
